ff26c0880cb1c21689ba88fc1383791dc6c79ec4
sharon3snc <sharonnunez89@gmail.com>
2023-02-04 14:22:58 +0100

    dudas

--- index.js
+++ index.js
@@ -14,6 +14,41 @@ var timezone = ...
 var WEATHER_API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum,windspeed_10m_max`;
 */
 
+function getLatitud() {
+    console.log("--- getting latitud");
+
+    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            console.log(data);
+            addUsersToPage(data);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+
+
+function getPostsByUser(userId) {
+    console.log("--- getting data for weather", city);
+
+    document.getElementById("resultado").innerHTML = 'Clima para: ' + city;
+
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum,windspeed_10m_max`)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            console.log(data);
+            addPostsToPage(data);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
 
 
 
